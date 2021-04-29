f4b7180627df8551d7af9a273cea21cd37a748d6
Ilmir <78535469+root0f@users.noreply.github.com>
2021-04-29 14:58:33 +0300

    without comment, only practice

--- src/component/Posts.js
+++ src/component/Posts.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import Post from "./Post";
 
-export default ({posts}) => {
-    return posts.map(post => <Post post={post} id={post}/>  )
+const Posts = ({myPosts}) => {
+    if (!myPosts.length){
+        return <p className="text-center">Пока ничего нет</p>
+    }
+    return myPosts.map(post => <Post post={post} key={post.id}/>  )
 }
+
+const mapStateToProps = state => {
+    console.log(state)
+    return {
+        myPosts: state.posts.posts
+    }
+}
+
+
+export default connect(mapStateToProps,null)(Posts)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import {render} from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {compose, createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import App from './App';
+import {rootReducer} from "./redux/rootReducer";
+import {forbiddenWordsMiddleware} from "./redux/middleware";
+import {sagaWatcher} from "./redux/sagas";
 
-render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk, forbiddenWordsMiddleware, saga
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+saga.run(sagaWatcher)
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+
+render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
